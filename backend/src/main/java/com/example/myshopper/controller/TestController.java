10d27c7a068b55c1b1ec92707756174b3dcf9d14
Eryk Tomaszewski <s217099@student.uek.krakow.pl>
2020-04-21 20:27:14 +0200

    Adding myBatis

--- backend/src/main/java/com/example/myshopper/controller/TestController.java
+++ backend/src/main/java/com/example/myshopper/controller/TestController.java
@@ -1,14 +1,25 @@
 package com.example.myshopper.controller;
 
 import com.example.myshopper.model.User;
+import com.example.myshopper.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private TestRepository testRepository;
+
+    @Autowired
+    public TestController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
     @GetMapping("test")
-    public User testEndpoint(){
-        return new User("erykulus", "ercio12@wp.pl");
+//    public User testEndpoint(){
+    public String testEndpoint(){
+        return testRepository.getTest();
+//        return new User("erykulus", "ercio12@wp.pl");
     }
 }
