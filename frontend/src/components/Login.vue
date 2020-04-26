3f298a44a1d424d495af494d0ae7646aeb9d1541
Slusarczyk Mateusz <mateusz.slusarczyk@bwigroup.com>
2020-04-26 15:03:54 +0200

    frontend project structure

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div class="form">
+    <h1>Logowanie</h1>
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Adres email:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Wprowadź email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Hasło:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          type="password"
+          v-model="form.password"
+          required
+          placeholder="Podaj hasło"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="light">Zaloguj</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$toastr.success('Zalogowano', 'Sukces!');
+    }
+  }
+};
+</script>
+
+<style scoped>
+@media (min-width: 1000px) {
+  .form {
+    width: 40%;
+    margin: auto;
+  }
+}
+@media (min-width: 800px) and (max-width: 999px){
+  .form {
+    width: 55%;
+    margin: auto;
+  }
+}
+@media (min-width: 600px) and (max-width: 799px){
+  .form {
+    width: 70%;
+    margin: auto;
+  }
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar class="fixed-top" toggleable="lg" type="light" variant="light">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/">
+            <font-awesome-icon icon="home"/> Strona główna
+          </b-nav-item>
+          <b-nav-item href="/login"><font-awesome-icon icon="sign-in-alt"/> Logowanie</b-nav-item>
+          <b-nav-item href="/register"><font-awesome-icon icon="user-plus"/> Rejestracja</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
